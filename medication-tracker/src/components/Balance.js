bf50be2b5a09e9d6bc3a1da61717fdcd8781ce6c
Syndicate555 <saziz028@uottawa.ca>
2020-06-21 05:24:48 -0400

    Create Balance.js

---
+++ medication-tracker/src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Balance = () => {
+  return (
+    <div>
+      <h4> You Current Medications</h4>
+      <li>Tylenol</li>
+      <li>Tylenol</li>
+      <li>Tylenol</li>
+    </div>
+  );
+};
