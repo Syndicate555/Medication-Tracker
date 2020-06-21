1f4c6723c17e7915eb6ba28dd3a9cb5c0d343d34
Syndicate555 <saziz028@uottawa.ca>
2020-06-21 05:14:04 -0400

    Update App.js

--- medication-tracker/src/App.js
+++ medication-tracker/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
